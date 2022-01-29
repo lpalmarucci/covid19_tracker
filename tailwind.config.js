1dcde008dda7a01c14c8e09a136e3ed1e058c360
lpalmarucci <lpalmarucci@gmail.com>
2022-01-29 17:55:27 +0100

    resetting darkMode to operating system

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,24 +6,23 @@ module.exports = {
   theme: {
     extend: {
       rotate: {
-        '135': '135deg'
+        135: "135deg",
       },
       animation: {
-        'slideUp': 'slideUp .3s linear forwards',
-        'slideDown': 'slideDown .3s linear forwards'
+        slideUp: "slideUp .3s linear forwards",
+        slideDown: "slideDown .3s linear forwards",
       },
       keyframes: {
         slideUp: {
-          '0%': { transform: 'translate(-50%,-30%)', opacity: '0' },
-          '100%': { transform: 'translate(-50%,-50%)', opacity: '1' }
+          "0%": { transform: "translate(-50%,-30%)", opacity: "0" },
+          "100%": { transform: "translate(-50%,-50%)", opacity: "1" },
         },
         slideDown: {
-          '0%': { transform: 'translate(-50%,-50%)', opacity: '1' },
-          '100%': { transform: 'translate(-50%,-30%)', opacity: '0' }
-        }
-      }
+          "0%": { transform: "translate(-50%,-50%)", opacity: "1" },
+          "100%": { transform: "translate(-50%,-30%)", opacity: "0" },
+        },
+      },
     },
   },
   plugins: [],
-  darkMode: 'class'
-}
+};
