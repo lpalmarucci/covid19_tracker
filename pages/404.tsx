f074f46065298ac15331682c114c669d8d25166f
lpalmarucci <lpalmarucci@gmail.com>
2022-01-30 14:39:09 +0100

    Enabled Dark Mode on 404 error page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,14 +1,25 @@
 import Header from "../components/Header";
-import Link from 'next/link'
+import Link from "next/link";
 
 export default function Custom404() {
-    return (
-        <>
-            <Header />
-            <div className="container flex justify-center flex-col items-center min-h-screen">
-                <h1 className="text-3xl font-bold mt-10">The resource you&apos;re trying to retrieve doesn&apos;t exists</h1>
-                <h3 className="text-lg text-center m-10">Return to <Link href="/" passHref><a className="transition-all hover:underline hover:font-bold">Home</a></Link></h3>
-            </div>
-        </>
-    )
+  return (
+    <>
+      <Header />
+      <div className="w-full bg-white dark:bg-gray-800 dark:text-white">
+        <div className="container flex justify-center flex-col items-center min-h-screen mx-auto">
+          <h1 className="text-3xl font-bold mt-10">
+            The resource you&apos;re trying to retrieve doesn&apos;t exists
+          </h1>
+          <h3 className="text-lg text-center m-10">
+            Return to{" "}
+            <Link href="/" passHref>
+              <a className="transition-all hover:underline hover:font-bold">
+                Home
+              </a>
+            </Link>
+          </h3>
+        </div>
+      </div>
+    </>
+  );
 }
