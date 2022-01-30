28b095928bd9c5f43c187264b85987a82a02cfec
lpalmarucci <lpalmarucci@gmail.com>
2022-01-30 14:35:25 +0100

    Aggiustata scritta copyright

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,7 +6,7 @@ export default function Footer({}: Props): ReactElement {
   return (
     <div className="w-full bg-white border-t-2  border-t-gray-200 dark:border-gray-500 p-6 text-center flex flex-col gap-y-1 items-center justify-center dark:bg-gray-800">
       <h1 className="text-2xl text-gray-500 dark:text-white">
-        © Copiright 2022 Il Principato{" "}
+        © Copyright 2022 Il Principato
       </h1>
       <span className="text-md text-gray-500 dark:text-gray-300">
         Fonte:{" "}
